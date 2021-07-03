b28ed21d286be59232b90984aa90592d9ad2a777
yui-git <yui.jump0329@gmail.com>
2021-07-04 02:20:51 +0900

    画面作成完了

---
+++ lib/mypage.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(height: 24.0),
+            Container(
+                width: double.infinity,
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Row(
+                  children: [
+                    Text(
+                      'id : 123456789　ようこそ！ 進研　太郎さん',
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                    Icon(
+                      Icons.male,
+                    )
+                  ],
+                )),
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https://www.benesse-i-career.co.jp/gps_academic/img/footer_log_gps_a.png',
+                      height: 65.0,
+                    ),
+                    Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Multiplication_Sign.svg/768px-Multiplication_Sign.svg.png',
+                      width: 20.0,
+                      height: 20.0,
+                    ),
+                    Image.network(
+                      'https://www.kaichi.ed.jp/archives/001/201801/large-5a67371656edf.png',
+                      height: 65.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'Colle×Pass',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.black,
+                        fontSize: 70.0,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                        shadows: <Shadow>[
+                          Shadow(
+                              color: Colors.black38,
+                              offset: Offset(3.0, 3.0),
+                              blurRadius: 3.0)
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(height: 10.0),
+                  SizedBox(
+                    height: 70.0,
+                    width: 400.0,
+                    child: RaisedButton(
+                      color: Colors.black,
+                      child: Text(
+                        "将来を見つけよう",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        // ボタン処理
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 70.0,
+                    width: 400.0,
+                    child: RaisedButton(
+                      color: Colors.black,
+                      child: Text(
+                        "お気に入り一覧へ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => SelectIndustry()));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
