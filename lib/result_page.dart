9fbd4f1d168b2081b7c25fe4b2fb1656d353e03d
nekonekosurflaptop <nekonekosurf@gmail.com>
2021-07-03 18:13:31 +0900

    first commit

---
+++ lib/result_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class ResultPage extends StatelessWidget {
+  // const ({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CAL'),
+      ),
+      body: Text('Hello'),
+    );
+  }
+}
